8d28273b87aeb9363348821b7428c25eed0c051e
unknown <https://github.com/eee595398>
2023-07-24 17:38:07 +0900

    1

---
+++ src/edu/kh/oop/practice/model/service/HeroService.java
@@ -0,0 +1,13 @@
+package edu.kh.oop.practice.model.service;
+
+import edu.kh.oop.practice.model.vo.Hero;
+
+public class HeroService {
+	
+	public void practice(){
+		Hero warrior = new Hero();
+		
+		warrior.setJob("워리어");
+	}
+
+}
---
+++ src/edu/kh/oop/practice/run/PracticeRun.java
@@ -0,0 +1,11 @@
+package edu.kh.oop.practice.run;
+
+import edu.kh.oop.practice.model.vo.Hero;
+
+public class PracticeRun {
+	public static void main(String[] args) {
+		Hero h =new Hero();
+		
+	}
+
+}
